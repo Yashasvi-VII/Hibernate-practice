d093f41c25465acce40fd428eb7232de4f068f42
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-04 01:10:06 +0530

    hibernate connected to db and got factory object

--- LearningHibernate1.3/FirstApp/src/main/java/com/yashasvi/App.java
+++ LearningHibernate1.3/FirstApp/src/main/java/com/yashasvi/App.java
@@ -1,13 +1,21 @@
 package com.yashasvi;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		Configuration cfg = new Configuration();
+		cfg.configure(); /* this reads hibernate.cfg.xml */
+
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+        System.out.print(factory);
+	}
 }
