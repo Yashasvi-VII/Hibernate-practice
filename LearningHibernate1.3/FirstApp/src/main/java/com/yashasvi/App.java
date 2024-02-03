ae116f48c77d60ea5c99a0acd4eed0fcf33c09d7
Yashasvi-VII <yashasvi9987@gmail.com>
2024-02-04 00:55:42 +0530

    Hibernate dependencies setup and hibernate xml based configurations

---
+++ LearningHibernate1.3/FirstApp/src/main/java/com/yashasvi/App.java
@@ -0,0 +1,13 @@
+package com.yashasvi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
